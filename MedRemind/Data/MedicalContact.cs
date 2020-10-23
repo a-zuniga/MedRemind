45c12ffd3881c78cf2d86318d8c12a55a6edd87c
Alejandro Zuniga <68042176+a-zuniga@users.noreply.github.com>
2020-10-23 03:14:07 -0400

    Added logic to add new contacts in the page and have it display

---
+++ MedRemind/Data/MedicalContact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedRemind.Data
+{
+    public class MedicalContact
+    {
+        public string office { get; set; }
+        public string name { get; set; }
+        public string specialty { get; set; }
+        public string phone { get; set; }
+    }
+}
---
+++ MedRemind/Data/PersonalContact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedRemind.Data
+{
+    public class PersonalContact
+    {
+        public string name { get; set; }
+        public string relation { get; set; }
+        public string phone { get; set; }
+    }
+}
