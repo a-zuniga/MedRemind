477ca64c98ad7e053eb5d2b6552c45aab55e9367
Alejandro Zuniga <68042176+a-zuniga@users.noreply.github.com>
2020-10-23 01:21:30 -0400

    bleh content

---
+++ MedRemind/Data/ContactList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedRemind.Data
+{
+    public class ContactList
+    {
+        public Doctor doctor { get; set; }
+        public Nurse nurse { get; set; }
+
+       public class Doctor
+        {
+            public string office { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string phoneNumber { get; set; }
+        }
+
+        public class Nurse
+        {
+            public string office { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string phoneNumber { get; set; }
+        }
+    }
+}
