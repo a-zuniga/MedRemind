80d448b815bd750028ce73a3c43455fa2ac972f6
ImpoliteArchitect <sanchez.navarrete@outlook.com>
2020-10-23 01:51:02 -0400

    I'm not sure what this one is

---
+++ MedRemind/Data/Medications.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace MedRemind.Data
+{
+    public class Medications
+    {
+        public string name { get; set; }
+        public int totalMeds { get; set; }
+        public string timeToTake { get; set; }
+        public string miligrams { get; set; }
+        public Boolean tookToday { get; set; }
+    }
+}
